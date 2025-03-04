f6a39fc34956c6bac999f0fbd6a0886fe11259d9
Balahari29 <147603813+Balahari29@users.noreply.github.com>
2025-03-04 16:05:05 +0530

    ReverseLine.java

--- ReverseLine.java
+++ ReverseLine.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Line {
+class Line {
     public static void main(String[] args) {
         System.out.println("Enter the line");
         Scanner sc= new Scanner(System.in);
